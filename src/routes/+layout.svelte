bc91f0b4b20e36d52dfab86b2a57774b4f9738af
Jesús Enrique Rascón <blograso@gmail.com>
2024-01-14 10:42:20 +0100

    fix upgrade tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,13 @@
 	import Container from '$lib/components/Container.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
-	import '../app.css';
+	import "../app.pcss";
 </script>
 
 <div class="selection:bg-accent selection:text-accent-content">
 	<Container>
-		<Header />
-		<slot />
-		<Footer />
+		<Header></Header>
+		<slot></slot>
+		<Footer></Footer>
 	</Container>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,60 +1,60 @@
 /** @type {import('tailwindcss').Config}*/
 const config = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Supreme'],
-        mono: ['Recursive'],
-        title: ['Whirly Birdie']
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            h1: {
-              'letter-spacing': '-3px'
-            },
-            h2: {
-              'letter-spacing': '-2px'
-            },
-            h3: {
-              'letter-spacing': '-2px'
-            }
-          }
-        }
-      }
-    }
-  },
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Supreme'],
+				mono: ['Recursive'],
+				title: ['Whirly Birdie']
+			},
+			typography: {
+				DEFAULT: {
+					css: {
+						h1: {
+							'letter-spacing': '-3px'
+						},
+						h2: {
+							'letter-spacing': '-2px'
+						},
+						h3: {
+							'letter-spacing': '-2px'
+						}
+					}
+				}
+			}
+		}
+	},
 
-  daisyui: {
-    logs: false,
-    themes: [
-      {
-        jesi: {
-          primary: '#a7f3d0',
-          'primary-focus': '#73E0B4',
-          'primary-content': '#021714',
-          secondary: '#a78bfa',
-          accent: '#fef9c3',
-          neutral: '#677471',
-          'base-100': '#021713',
-          'base-200': '#001410',
-          'base-300': '#010D0B',
-          'base-content': '#fff',
-          info: '#3abff8',
-          success: '#4ade80',
-          warning: '#fbbd23',
-          error: '#f87272'
-        }
-      }
-    ]
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-    require('daisyui')
-  ]
+	daisyui: {
+		logs: false,
+		themes: [
+			{
+				jesi: {
+					primary: '#a7f3d0',
+					'primary-focus': '#73E0B4',
+					'primary-content': '#021714',
+					secondary: '#a78bfa',
+					accent: '#fef9c3',
+					neutral: '#677471',
+					'base-100': '#021713',
+					'base-200': '#001410',
+					'base-300': '#010D0B',
+					'base-content': '#fff',
+					info: '#3abff8',
+					success: '#4ade80',
+					warning: '#fbbd23',
+					error: '#f87272'
+				}
+			}
+		]
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/container-queries'),
+		require('daisyui')
+	]
 };
 
 module.exports = config;
