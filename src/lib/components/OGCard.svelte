8c1083d6c6d660a3a6f571f9a01413a4ee677c74
Jesús Enrique Rascón <blograso@gmail.com>
2024-02-17 18:42:00 +0100

    improve card

--- src/lib/components/OGCard.svelte
+++ src/lib/components/OGCard.svelte
@@ -3,13 +3,17 @@
 	export let desc: string;
 </script>
 
-<main class="h-full w-full bg-[#021713] text-center">
+<main
+	style="background-image: linear-gradient(to bottom, #021713, #001410)"
+	class="h-full w-full text-center"
+>
 	<div class="mx-auto flex max-w-[80%] flex-col">
-		<div class="mx-auto my-20 w-fit text-7xl font-bold text-white">
+		<div class="mx-auto my-20 w-fit text-[120px] font-bold tracking-tighter text-white">
 			{title}
 		</div>
-		<div class="mx-auto mt-10 w-fit text-center text-3xl font-thin text-white opacity-80">
+		<div class="mx-auto mt-10 w-fit text-center text-[50px] font-thin text-white opacity-50">
 			{desc}
 		</div>
+		<div class="mx-auto mt-32 w-fit font-mono text-white">jesirgb.com</div>
 	</div>
 </main>
