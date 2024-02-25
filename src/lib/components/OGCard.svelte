5906e125040a5e78222d06f4084045c965afce60
Jesús Enrique Rascón <blograso@gmail.com>
2024-02-25 12:54:40 +0100

    ogcard

--- src/lib/components/OGCard.svelte
+++ src/lib/components/OGCard.svelte
@@ -5,10 +5,10 @@
 
 <main style="background-image: linear-gradient(to bottom, #021713, #001410)" class="h-full w-full">
 	<div class="mx-10 flex max-w-[80%] flex-col">
-		<div class="my-20 w-fit font-title text-[120px] font-bold tracking-tighter text-white">
+		<div class="mt-20 w-fit font-title text-[120px] font-bold tracking-tighter text-white">
 			{title}
 		</div>
-		<div class="mt-10 w-fit text-[50px] font-thin text-white opacity-50">
+		<div class="w-fit text-[50px] font-thin text-white opacity-50">
 			{desc}
 		</div>
 		<div class="mt-32 w-fit font-mono text-white">jesirgb.com</div>
