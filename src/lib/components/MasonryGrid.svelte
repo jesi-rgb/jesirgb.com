c021164217e6ce36c78a916064331b71d25cad6e
Jesús Enrique Rascón <blograso@gmail.com>
2024-09-14 23:39:43 +0200

    move metatags to their place

--- src/lib/components/MasonryGrid.svelte
+++ src/lib/components/MasonryGrid.svelte
@@ -35,44 +35,7 @@
 		const img = new Image();
 		img.src = imageSrc; // Browser starts downloading the image
 	};
-
-	let thumbUrl = new URL('https://www.jesirgb.com/blog/thumbnails');
-	thumbUrl.searchParams.append('title', 'Pictures');
-	thumbUrl.searchParams.append(
-		'desc',
-		'Selected pictures from Jesús Rascón, captured and edited on the go.'
-	);
 </script>
-
-<MetaTags
-	keywords={['pictures', 'photography', 'iphone']}
-	title="Pictures — Jesús Rascón"
-	description="Selected pictures from Jesús Rascón, captured and edited on the go."
-	twitter={{
-		handle: '@jesi_rgb',
-		cardType: 'summary_large_image',
-		title: 'Pictures — Jesús Rascón',
-		description: 'Selected pictures from Jesús Rascón, captured and edited on the go.',
-		image: thumbUrl.toString(),
-		imageAlt: 'Pictures'
-	}}
-	openGraph={{
-		type: 'article',
-		url: thumbUrl.toString(),
-		title: 'Pictures',
-		images: [
-			{
-				url: thumbUrl.toString(),
-				width: 800,
-				height: 600
-			}
-		],
-		description: 'Selected pictures from Jesús Rascón, captured and edited on the go.',
-		article: {
-			tags: ['pictures', 'photography', 'iphone']
-		}
-	}}
-/>
 
 <svelte:window on:keydown={handleKeyDown} />
 
--- src/routes/pictures/+page.svelte
+++ src/routes/pictures/+page.svelte
@@ -2,6 +2,7 @@
 	import MasonryGrid from '$lib/components/MasonryGrid.svelte';
 	import MegaTitle from '$lib/components/MegaTitle.svelte';
 	import { onMount } from 'svelte';
+	import { MetaTags } from 'svelte-meta-tags';
 
 	let images;
 
@@ -11,12 +12,47 @@
 		images = data;
 	};
 
-	$: console.log(images);
-
 	onMount(() => {
 		fetchImages();
 	});
+
+	let thumbUrl = new URL('https://www.jesirgb.com/blog/thumbnails');
+	thumbUrl.searchParams.append('title', 'Pictures');
+	thumbUrl.searchParams.append(
+		'desc',
+		'Selected pictures from Jesús Rascón, captured and edited on the go.'
+	);
 </script>
+
+<MetaTags
+	keywords={['pictures', 'photography', 'iphone']}
+	title="Pictures — Jesús Rascón"
+	description="Selected pictures from Jesús Rascón, captured and edited on the go."
+	twitter={{
+		handle: '@jesi_rgb',
+		cardType: 'summary_large_image',
+		title: 'Pictures — Jesús Rascón',
+		description: 'Selected pictures from Jesús Rascón, captured and edited on the go.',
+		image: thumbUrl.toString(),
+		imageAlt: 'Pictures'
+	}}
+	openGraph={{
+		type: 'article',
+		url: thumbUrl.toString(),
+		title: 'Pictures',
+		images: [
+			{
+				url: thumbUrl.toString(),
+				width: 800,
+				height: 600
+			}
+		],
+		description: 'Selected pictures from Jesús Rascón, captured and edited on the go.',
+		article: {
+			tags: ['pictures', 'photography', 'iphone']
+		}
+	}}
+/>
 
 <MegaTitle>Pictures</MegaTitle>
 
