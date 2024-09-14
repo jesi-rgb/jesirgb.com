bd7ceb869825384cd6bfff5f1c733dba61f48a50
Jesús Enrique Rascón <blograso@gmail.com>
2024-09-14 23:29:04 +0200

    add meta tags

--- src/lib/components/MasonryGrid.svelte
+++ src/lib/components/MasonryGrid.svelte
@@ -3,6 +3,7 @@
 	import Camera from 'phosphor-svelte/lib/Camera';
 	import FilmStrip from 'phosphor-svelte/lib/FilmStrip';
 	import Aperture from 'phosphor-svelte/lib/Aperture';
+	import { MetaTags } from 'svelte-meta-tags';
 
 	// Props: array of image objects
 	export let images;
@@ -34,7 +35,44 @@
 		const img = new Image();
 		img.src = imageSrc; // Browser starts downloading the image
 	};
+
+	let thumbUrl = new URL('https://www.jesirgb.com/blog/thumbnails');
+	thumbUrl.searchParams.append('title', 'Pictures');
+	thumbUrl.searchParams.append(
+		'subtitle',
+		'Selected pictures from Jesús Rascón, captured and edited on the go.'
+	);
 </script>
+
+<MetaTags
+	keywords={['pictures', 'photography', 'iphone']}
+	title="Pictures — Jesús Rascón"
+	description="Selected pictures from Jesús Rascón, captured and edited on the go."
+	twitter={{
+		handle: '@jesi_rgb',
+		cardType: 'summary_large_image',
+		title: 'Pictures — Jesús Rascón',
+		description: 'Selected pictures from Jesús Rascón, captured and edited on the go.',
+		image: thumbUrl.toString(),
+		imageAlt: 'Pictures'
+	}}
+	openGraph={{
+		type: 'article',
+		url: thumbUrl.toString(),
+		title: 'Pictures',
+		images: [
+			{
+				url: thumbUrl.toString(),
+				width: 800,
+				height: 600
+			}
+		],
+		description: 'Selected pictures from Jesús Rascón, captured and edited on the go.',
+		article: {
+			tags: ['pictures', 'photography', 'iphone']
+		}
+	}}
+/>
 
 <svelte:window on:keydown={handleKeyDown} />
 
