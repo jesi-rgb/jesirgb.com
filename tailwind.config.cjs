5d759535c073fe23464a9de50eed6c6d55bf445d
Jesús Enrique Rascón <blograso@gmail.com>
2025-05-17 17:16:04 +0200

    yea

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,47 @@
 /** @type {import('tailwindcss').Config}*/
+
+// daisyui: {
+// 	logs: false,
+// 	themes: [
+// 		{
+// 			light: {
+// 				primary: '#226E48',
+// 				'primary-focus': '#042B24',
+// 				'primary-content': '#F6FFFB',
+// 				secondary: '#a78bfa',
+// 				accent: '#D1BC03',
+// 				neutral: '#677471',
+// 				'base-100': '#F6FFFB',
+// 				'base-200': '#FAFFFD',
+// 				'base-300': '#E5FDF2',
+// 				'base-content': '#042B24',
+// 				info: '#3abff8',
+// 				success: '#4ade80',
+// 				warning: '#fbbd23',
+// 				error: '#f87272'
+// 			}
+// 		},
+// 		{
+// 			dark: {
+// 				primary: '#a7f3d0',
+// 				'primary-focus': '#73E0B4',
+// 				'primary-content': '#021714',
+// 				secondary: '#a78bfa',
+// 				accent: '#fef9c3',
+// 				neutral: '#677471',
+// 				'base-100': '#021713',
+// 				'base-200': '#001410',
+// 				'base-300': '#010D0B',
+// 				'base-content': '#fff',
+// 				info: '#3abff8',
+// 				success: '#4ade80',
+// 				warning: '#fbbd23',
+// 				error: '#f87272'
+// 			}
+// 		},
+// 		'cupcake'
+// 	]
+// },
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
@@ -12,52 +55,9 @@ const config = {
 		}
 	},
 
-	daisyui: {
-		logs: false,
-		themes: [
-			{
-				light: {
-					primary: '#226E48',
-					'primary-focus': '#042B24',
-					'primary-content': '#F6FFFB',
-					secondary: '#a78bfa',
-					accent: '#D1BC03',
-					neutral: '#677471',
-					'base-100': '#F6FFFB',
-					'base-200': '#FAFFFD',
-					'base-300': '#E5FDF2',
-					'base-content': '#042B24',
-					info: '#3abff8',
-					success: '#4ade80',
-					warning: '#fbbd23',
-					error: '#f87272'
-				}
-			},
-			{
-				dark: {
-					primary: '#a7f3d0',
-					'primary-focus': '#73E0B4',
-					'primary-content': '#021714',
-					secondary: '#a78bfa',
-					accent: '#fef9c3',
-					neutral: '#677471',
-					'base-100': '#021713',
-					'base-200': '#001410',
-					'base-300': '#010D0B',
-					'base-content': '#fff',
-					info: '#3abff8',
-					success: '#4ade80',
-					warning: '#fbbd23',
-					error: '#f87272'
-				}
-			},
-			'cupcake'
-		]
-	},
 	plugins: [
 		require('@tailwindcss/typography'),
 		require('@tailwindcss/container-queries'),
-		require('daisyui')
 	]
 };
 
