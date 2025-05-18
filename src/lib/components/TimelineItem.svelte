8ffdf240ee05fff49bc0ff718bf06df1a2739947
Jesús Enrique Rascón <blograso@gmail.com>
2025-05-18 20:18:14 +0200

    smol details

--- src/lib/components/TimelineItem.svelte
+++ src/lib/components/TimelineItem.svelte
@@ -6,12 +6,7 @@
 		content?: import('svelte').Snippet;
 	}
 
-	let {
-		title,
-		date,
-		subtitle,
-		content
-	}: Props = $props();
+	let { title, date, subtitle, content }: Props = $props();
 </script>
 
 <div class="group relative py-6 pl-8 sm:pl-32">
@@ -19,10 +14,10 @@
 	<div class="whirly mb-1 text-3xl sm:mb-0">{@render title?.()}</div>
 	<!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) -->
 	<div
-		class="mb-1 flex flex-col items-start before:absolute before:left-2 before:h-full before:-translate-x-1/2 before:translate-y-3 before:self-start before:bg-linear-to-b before:from-primary/40 before:to-primary-content/40 before:px-px after:absolute after:left-2 after:box-content after:h-2 after:w-2 after:-translate-x-1/2 after:translate-y-1.5 after:rounded-full after:border-2 after:border-base-content after:bg-base-100 group-last:before:hidden sm:flex-row sm:before:left-0 sm:before:ml-26 sm:after:left-0 sm:after:ml-26"
+		class="before:from-primary/40 before:to-primary-content/40 after:border-base-content after:bg-base-100 mb-1 flex flex-col items-start before:absolute before:left-2 before:h-full before:-translate-x-1/2 before:translate-y-3 before:self-start before:bg-linear-to-b before:px-px group-last:before:hidden after:absolute after:left-2 after:box-content after:h-2 after:w-2 after:-translate-x-1/2 after:translate-y-1.5 after:rounded-full after:border-2 sm:flex-row sm:before:left-0 sm:before:ml-26 sm:after:left-0 sm:after:ml-26"
 	>
 		<time
-			class="w-22 badge badge-primary left-0 mb-3 inline-flex h-6 translate-y-0.5 font-mono text-xs sm:absolute sm:mb-0"
+			class="badge badge-primary left-0 mb-3 inline-flex h-6 translate-y-0.5 font-mono text-xs sm:absolute sm:mb-0"
 			>{@render date?.()}</time
 		>
 		<div class="subtitle text-xl font-semibold">{@render subtitle?.()}</div>
