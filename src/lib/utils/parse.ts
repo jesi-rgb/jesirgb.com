116286ac7c3f2a182724565af61263a3385cc46f
Jesús Enrique Rascón <blograso@gmail.com>
2025-10-01 10:44:10 +0200

    build

--- src/lib/utils/parse.ts
+++ src/lib/utils/parse.ts
@@ -5,7 +5,6 @@ const factor = 1.5;
 export function parseQuery(query) {
   const title = query.get('title') ?? undefined;
   const desc = query.get('desc') ?? undefined;
-  // only allow seeds in the random word list
   const width = query.get('w') ?? WIDTH;
   const height = query.get('h') ?? HEIGHT;
   return { title: title, desc: desc, width: +width * factor, height: +height * factor };
